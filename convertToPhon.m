5b5960ef52ef3b4f801795e340b0c7bb69d40e97
JEBlack <jenah1@me.com>
2019-01-20 20:24:57 -0600

    updated comments

--- convertToPhon.m
+++ convertToPhon.m
@@ -15,25 +15,25 @@ words=cleanUp(words); %remove any words that don't fit the criteria
 
 % Represented in a large array
 % The words are concatinated onto each other, adding to the right
-% New word every 11 columns.
-% Was this what we talked about last week?
+    % New word every 11 columns.
+    % Was this what we talked about last week?
 
 repArrayRight=[];
 for i=1:200
     repArrayRight=[repArrayRight,fPhon(words(i,2))];
 end
 
-% represented in a container, key is char array of ortho, value is array of
+% Represented in a container, key is char array of ortho, value is array of
 % phonological features
-% access orthography array with repMap.keys
-% cannot iterate through values without using the key.
+    % access orthography array with repMap.keys
+    % cannot iterate through values without using the key.
 
 repMap=containers.Map('keytype','char','ValueType','any'); 
 for i=1:200 
     repMap(char(words{i,1}))=fPhon(words(i,2));
 end
 
-% represented in a 3d array. Each page is the feature representations for a
+% Represented in a 3d array. Each page is the feature representations for a
 % single word
     % iterate through words with repArray(:,:,index)
     % Might be useful if we want to access certain features easily-- ex. access
